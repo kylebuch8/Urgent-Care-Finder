fe2746cf424f31d798954f2b3e8c492c136a69cd
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2013-01-10 11:30:32 -0500

    built an autocomplete function that is powered by google

--- js/urgentcarefinder.js
+++ js/urgentcarefinder.js
@@ -23,6 +23,29 @@
 
 	// controller for the location input form
 	app.controller("FormInput", ["$scope", function FormInput($scope) {
+		/*
+		 * build a places autocomplete that is powered by google
+		 * https://developers.google.com/maps/documentation/javascript/places#places_autocomplete
+		 *
+		 * we'll restrict the autocomplete to the united states.
+		 * if the user uses an autocompleted location, we'll need
+		 * to figure out what to do with the place that is returned
+		 */
+		var initAutocomplete = function() {
+			var input = document.getElementById("locationInput"),
+				options = {
+					componentRestrictions : {
+						"country" : "us"
+					}
+				},
+				autocomplete = new google.maps.places.Autocomplete(input, options);
+
+			google.maps.event.addListener(autocomplete, "place_changed", function() {
+				var place = autocomplete.getPlace();
+				console.log("place", place);
+			});
+		};
+
 		$scope.submit = function() {
 			/*
 			 * if we have a location, we'll emit an event so anyone
@@ -32,5 +55,7 @@
 				$scope.$emit("LocationEntered", this.location);
 			}
 		};
+
+		initAutocomplete();
 	}]);
 })();
