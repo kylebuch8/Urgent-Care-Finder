336f2c4396cd551bc8538aeb07c02b4b3d9e18c1
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2013-01-12 11:23:54 -0500

    created a map controller

--- js/urgentcarefinder.js
+++ js/urgentcarefinder.js
@@ -88,4 +88,20 @@
 		];
 	}]);
 
+	// controller for the map view
+	app.controller("UrgentCareMapCtrl", ["$scope", function UrgentCareMapCtrl($scope) {
+		var initMap = function() {
+			/*
+			 * the map is centered on an area near Jordan Lake
+			 */
+			var mapOptions = {
+					center : new google.maps.LatLng(35.759573,-79.0193),
+					zoom : 8,
+					mapTypeId : google.maps.MapTypeId.ROADMAP
+				},
+				map = new google.maps.Map(document.getElementById("map"), mapOptions);
+		};
+
+		initMap();
+	}]);
 })();
