c987b39f55bd8c57941d14aa6bfee55244511870
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2013-01-16 17:38:41 -0500

    created a service to handle the fetching of the data

---
+++ app/js/services/centersservice.js
@@ -0,0 +1,36 @@
+(function() {
+	"use strict";
+
+	/*
+	 * we'll use this service to fetch all of the urgent care centers
+	 * that are close to the location that is passed
+	 *
+	 * this service requires that a location object is passed to it
+	 * in the following format:
+	 *
+	 * {
+	 *   lat : xx,
+	 *   lng : yy
+	 * }
+	 */
+	angular.module("urgentCareFinder").factory("CentersService", ["$rootScope", "$http", function($rootScope, $http) {
+		var fetch = function(location) {
+			var method = "GET",
+				latlngStr = location.lat + "," + location.lng,
+				url = "http://localhost:3000/centers/?latlng=" + latlngStr;
+
+			$http({
+				method : method,
+				url : url
+			}).success(function(data, status) {
+				$rootScope.$broadcast("ResultsListUpdated", data);
+			}).error(function(data, status) {
+				console.log("error", status);
+			});
+		};
+
+		return {
+			fetch : fetch
+		};
+	}]);
+})();
