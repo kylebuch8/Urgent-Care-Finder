edd599cfe27e7451fb3e4d0125a8cc9908869e08
Kyle Buchanan <kyle.buchanan@bcbsnc.com>
2013-01-22 13:21:37 -0500

    added filter for phone numbers and their buttons
    need to strip out dashes in phone numbers

--- app/js/urgentcarefinder.js
+++ app/js/urgentcarefinder.js
@@ -108,5 +108,23 @@
 		$(window).resize(resizePanes);
 		resizePanes();
 		initGeolocation();
+	})
+	/*
+	 * create a filter to remove all dashes for phone
+	 * numbers and put a 1 at the beginning of the string.
+	 * this is needed for the phone buttons href
+	 *
+	 * tel://xxx-yyy-zzzz
+	 *
+	 * becomes
+	 *
+	 * tel://1xxxyyyzzzz
+	 */
+	.filter("phone", function() {
+		return function(phone) {
+			phone = phone.replace(/-/g, "");
+
+			return "1" + phone;
+		};
 	});
 })();
